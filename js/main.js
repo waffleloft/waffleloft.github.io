9d5ae3b4d84a5f8edb3e49be98b6f2f7b5f0e130
Chase Hutchins <syntaqx@gmail.com>
2013-10-04 19:19:45 -0600

    preparing for the new design

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+
+  // ...
+  setTimeout(function () {
+
+    $('.coming-soon').animate({
+      'margin-top': 300,
+      'opacity': 1
+    }, 'slow')
+
+  }, 1500)
+
+})
